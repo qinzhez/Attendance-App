d4258f7c8e8c23f77e7494c8d6451bdd4a75c7d8
qinzh <qinzhez@gmail.com>
2018-10-23 14:51:03 -0400

    [FrontEnd] route module updated

--- dev/src/main/java/com/attendU/dev/microservices/user/Token.java
+++ dev/src/main/java/com/attendU/dev/microservices/user/Token.java
@@ -1,5 +1,12 @@
 package com.attendU.dev.microservices.user;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+import java.util.Base64.Encoder;
+import java.util.Calendar;
+import java.util.Date;
+
 import com.attendU.dev.microservices.bean.TokenBean;
 
 import lombok.Getter;
@@ -11,14 +18,43 @@ public class Token {
 	private String token;
 	private String secret;
 
+	@Getter
+	private Date createTime;
+	@Getter
+	private Date expiration;
+
 	public Token(String sec, long id) {
 		uid = id;
 		secret = sec;
+		createTime = new Date();
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(createTime);
+		calendar.add(Calendar.DAY_OF_MONTH, 1);
+		expiration = calendar.getTime();
+		token = tokenGenerate(Long.toString(createTime.getTime()), Long.toString(uid));
 	}
 
 	public Token(String sec, TokenBean tokenBean) {
 		uid = tokenBean.getUid();
 		token = tokenBean.getToken();
+		secret = sec;
+		createTime = tokenBean.getCreateTime();
+		expiration = tokenBean.getExpiration();
 	}
 
+	private String tokenGenerate(String time, String uid) {
+		String token = time+secret+uid;
+        String tokenMd5="";
+        try {
+            MessageDigest md = MessageDigest.getInstance("md5");
+            byte[] md5 = md.digest(token.getBytes());
+            Encoder base = Base64.getEncoder();
+            tokenMd5 = base.encodeToString(md5);
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        return tokenMd5;
+	}
 }
--- dev/webapp/app/app.config.js
+++ dev/webapp/app/app.config.js
@@ -4,42 +4,25 @@ angular.
   module('attendU').
   config(['$locationProvider' ,'$routeProvider',
     function config($locationProvider, $routeProvider) {
-      $locationProvider.html5Mode(true);
       //$locationProvider.hashPrefix('!');
 
       $routeProvider.
-//		when('', {
-//	      templateUrl: 'index.html'
-//		}).
+		when('/index', {
+	      templateUrl: 'dashboard/index.html'
+		}).
         when('/home', {
           templateUrl: 'dashboard/home.html'
         }).
-//		when('/login', {
-//		  controller: 'LoginController',
-//          templateUrl: 'login/login.view.html',
-//          controllerAs: 'vm'
-//		}).
-//		when('/register', {
-//		  controller: 'RegisterController',
-//          templateUrl: 'login/register.view.html',
-//          controllerAs: 'vm'
-//		}).
-        otherwise({redirectTo: '/home'});
-  
-      //首页
-      // $stateProvider.state('Login', {
-            // url: '/',
-            // templateUrl: './Views/Login.html',
-            // controller: 'Login_controller',
-            // controllerUrl: './controller/Login-controller.js'
-      // });
-  
-      //个人信息页
-      // $stateProvider.state('Login.Information', {
-          // url: 'Information/:ID',
-          // templateUrl: './Views/Info/Information.html',
-          // controller: 'Information_controller',
-          // controllerUrl: './controller/Info/Information-controller.js'
-      // });
+		when('/login', {
+		  controller: 'LoginController',
+         templateUrl: 'login/login.view.html',
+         controllerAs: 'vm'
+		}).
+		when('/register', {
+		  controller: 'RegisterController',
+         templateUrl: 'login/register.view.html',
+         controllerAs: 'vm'
+		}).
+        otherwise({redirectTo: '/index'});
     }
   ]);
