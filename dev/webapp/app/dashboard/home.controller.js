420c0c825e3aebe4819b8135cfcacb4ac419cde8
qinzh <qinzhez@gmail.com>
2018-11-24 00:44:04 -0500

    [bug#74]similar problem and solved for home redirection

--- dev/webapp/app/dashboard/home.controller.js
+++ dev/webapp/app/dashboard/home.controller.js
@@ -5,13 +5,18 @@
         .module('attendU')
         .controller('HomeController', HomeController);
 
-    HomeController.$inject = ['$location', 'AuthenticationService', 'FlashService'];
-    function HomeController($location, AuthenticationService, FlashService) {
+    HomeController.$inject = ['$location', 'AuthenticationService', 'FlashService', '$q'];
+    function HomeController($location, AuthenticationService, FlashService, $q) {
         var vm = this;
 
 
         (function initController() {
-            AuthenticationService.ValidCredentials(function(result){
+            var deferred = $q.defer();
+            var promise = deferred.promise;
+
+            AuthenticationService.ValidCredentials(deferred);
+
+            promise.then(function(result){
                 if(!result){
                     $location.path('/');
                 }
